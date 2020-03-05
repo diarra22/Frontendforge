62cc1a7814f49e302c8757247df6b77dd0e4c428
diarra22 <abdourahamane.diarra@viacesi.fr>
2020-03-05 23:21:05 +0100

    commit ajout partie jeu morpion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { HomeComponent } from './home/home.component';
+import { UpdateComponent } from './update/update.component';
+import { AuthGaurdService } from './service/auth-gaurd.service';
+import { GameComponent } from './game/game.component';
+const routes: Routes = [
+  { path: '', component: AuthenticationComponent },
+  { path: 'home', component: HomeComponent, canActivate:[AuthGaurdService] },
+  { path: 'update', component: UpdateComponent, canActivate:[AuthGaurdService] },
+  { path: 'game', component: GameComponent, canActivate:[AuthGaurdService] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { BasicAuthHtppInterceptorServiceService} from './service/basic-auth-htpp-interceptor-service.service';
+import { UpdateComponent } from './update/update.component';
+import { GameComponent } from './game/game.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatGridListModule  } from '@angular/material/grid-list'
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AuthenticationComponent,
+    HomeComponent,
+    UpdateComponent,
+    GameComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatSnackBarModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass: BasicAuthHtppInterceptorServiceService,
+      multi   : true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
